ea47a9db5ff6e181b2933d9448cf85060d8959dc
zb666 <1360799362@qq.com>
2019-02-23 21:20:54 +0800

    父控件传递给子控件 采用this.props.xxx

--- rn_meituan_project/MeiTuan/js/Find/find.js
+++ rn_meituan_project/MeiTuan/js/Find/find.js
@@ -11,19 +11,45 @@ import React, {Component} from 'react';
 import {Platform, StyleSheet, Text, View} from 'react-native';
 
 
+//父组件
 export default class Find extends Component<Props> {
     render() {
         return (
             <View>
-                <Text>
-                    Find页面
-                </Text>
+                <Text>Find页面</Text>
+                <FatherComponent name="哈哈，这是父控件传递的属性，子控件可以通过this.props.xxx 进行获取"/>
+            </View>
+        );
+    }
+}
+
+class SonComponent extends Component {
+    render() {
+        return (
+            <View style={styles.container}>
+                <Text style={styles.welcome}>{this.props.name}的孩子组件</Text>
+            </View>
+        );
+    };
+}
+
+class FatherComponent extends Component {
+    render() {
+        //父组件委托给主组件 渲染带有子组件的ui布局代码
+        return (<View style={{flex: 1, alignItems: 'center'}}>
+                <SonComponent name={this.props.name}/>
             </View>
         );
     }
 }
 
 const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        justifyContent: 'center',
+        alignItems: 'center',
+        backgroundColor: '#F5FCFF',
+    },
     welcome: {
         fontSize: 20,
         textAlign: 'center',
--- rn_meituan_project/MeiTuan/js/Luancher/luancher.js
+++ rn_meituan_project/MeiTuan/js/Luancher/luancher.js
@@ -8,7 +8,8 @@
  */
 
 import React, {Component} from 'react';
-import {Platform, StyleSheet, Text, View,TextInput,Image,StatusBar} from 'react-native';
+import {Platform, StyleSheet, Text, View, TextInput, Image, StatusBar} from 'react-native';
+
 var Demensions = require('Dimensions');
 
 import Main from '../Main/main';
@@ -18,37 +19,33 @@ var {width, height} = Demensions.get('window');
 export default class Luancher extends Component<Props> {
     //onResume  1次
     componentDidMount(): void {
-        setTimeout(()=>{
+        setTimeout(() => {
             this.props.navigator.replace({
-                component:Main
+                component: Main
             })
-        },100)
+        }, 100)
     }
 
     render() {
-
         return (
             <View>
                 <StatusBar
                     backgroundColor='rgba(0,0,0,0.0)'
-                    hidden={true}
-                />
-                <Image source={require('../../res/images/luancher.png')}  style={styles.luanchImageStyle}/>
+                    hidden={true}/>
+                <Image source={require('../../res/images/luancher.png')} style={styles.luanchImageStyle}/>
             </View>
-           );
+        );
     }
-
-
 }
 
 const styles = StyleSheet.create({
-    luanchImageStyle:{
-        width:width,
-        height:height,
+    luanchImageStyle: {
+        width: width,
+        height: height,
         //不加这句，就是按照屏幕高度自适应
         //加上这几，就是按照屏幕自适应（拉伸整个屏幕）
-        resizeMode:'cover',
+        resizeMode: 'cover',
         //祛除内部元素的白色背景
-        backgroundColor:'rgba(0,0,0,0)',
+        backgroundColor: 'rgba(0,0,0,0)',
     }
 });
