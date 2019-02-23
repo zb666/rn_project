7ce85ce68be2b7209172511a729ca326b458892c
zb666 <1360799362@qq.com>
2019-02-23 21:53:55 +0800

    父子逆传 通过父控件给与的bind通道

--- rn_meituan_project/MeiTuan/js/Find/find.js
+++ rn_meituan_project/MeiTuan/js/Find/find.js
@@ -15,10 +15,7 @@ import {Platform, StyleSheet, Text, View} from 'react-native';
 export default class Find extends Component<Props> {
     render() {
         return (
-            <View>
-                <Text>Find页面</Text>
-                <FatherComponent name="哈哈，这是父控件传递的属性，子控件可以通过this.props.xxx 进行获取"/>
-            </View>
+            <FatherComponent/>
         );
     }
 }
@@ -39,25 +36,50 @@ class SonComponent extends Component {
         })
     }
 
+    makeMoney(money) {
+        this.props.reveiveMoney(money)
+    }
+
     render() {
         return (
             <View style={styles.container}>
                 <Text style={styles.welcome}>{this.props.name}的孩子组件</Text>
                 <Text>总共收到了来自于父亲的 {this.state.money} 生活费</Text>
+                <Text onPress={() => this.makeMoney.bind(this, 100)}>点击挣100块钱</Text>
             </View>
         );
     };
 }
 
 class FatherComponent extends Component {
+
+    constructor(props) {
+        super(props);
+        {
+            this.state = {
+                money: 0
+            }
+        }
+    }
+
     render() {
         //父组件委托给主组件 渲染带有子组件的ui布局代码
-        return (<View style={{flex: 1, alignItems: 'center'}}>
+        return (
+            <View style={{flex: 1, alignItems: 'center'}}>
+
                 <SonComponent name={this.props.name}/>
+
                 <SonComponent ref="son"/>
+
                 <Text style={{marginTop: 100}} onPress={() => {
                     this.refs.son.reveiveMoney(1000)
-                }}>发生活费了</Text>
+                }}>发生活费了
+                </Text>
+
+                {/*<SonComponent reveiveMoney={this.reveiveMoney.bind(this)}/>*/}
+
+                <Text style={{marginBottom: 50}}>爸爸收到了通过绑定的子控件的bind方法而收到的100块钱{this.state.money}</Text>
+
             </View>
         );
     }
